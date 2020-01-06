0813d153b0f319a4510f0804b894917baac8811f
Daniel <danescore@gmail.com>
2020-01-06 01:46:09 +0100

    login page and edit page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {LogInLayoutComponent} from './log-in-page/log-in-layout/log-in-layout.co
 import {EditMessageLayoutComponent} from './edit-message-page/edit-message-layout/edit-message-layout.component';
 
 const routes: Routes = [
-  { path: 'log-in-page', component: LogInLayoutComponent },
+  { path: 'log-in', component: LogInLayoutComponent },
   { path: 'messages', component: MessageListLayoutComponent },
   { path: 'messages/:id', component: EditMessageLayoutComponent },
   { path: '**', redirectTo: '/messages' }
